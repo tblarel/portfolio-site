87172b72e4c3b02fb79d13352063e845ddb493d6
t_blarel <tblarel@gmail.com>
2019-05-29 17:15:52 -0700

    Work on subpages and modals

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -2,7 +2,11 @@ var slideIndex = 1;
 showDivs(slideIndex,'one');
 showDivs(slideIndex, 'two');
 showDivs(slideIndex, 'three');
+// showDivs(slideIndex, 'four');
 showDivs(slideIndex, 'five');
+showDivs(slideIndex, 'six');
+showDivs(slideIndex, 'seven');
+showDivs(slideIndex, 'eight');
 
 function plusDivs(n, idx) {
     showDivs(slideIndex += n,idx);
@@ -11,10 +15,12 @@ function plusDivs(n, idx) {
 function showDivs(n,idx) {
     var i;
     var x = document.getElementsByClassName("mySlides "+idx);
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    if(x.length != 0) {
+        if (n > x.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = x.length };
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex - 1].style.display = "block";
     }
-    x[slideIndex - 1].style.display = "block";
 }
